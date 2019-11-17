0f9b6f0fc54f5d7e96bdd95fae89708cd27b96ad
Uwem Uke <slyart147@gmail.com>
2019-11-17 12:09:15 +0100

    get users activated

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,7 @@ const auth = require('../middleware/auth');
 
 const userCtrl = require('../controllers/user');
 
-//router.get('/users', userCtrl.getUsers);
+router.get('/users', userCtrl.getUsers);
 router.post('/auth/create-user', auth, userCtrl.createUser);
 router.post('/auth/signin', userCtrl.login);
 
