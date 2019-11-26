3b9b0a2fc2943d8788d51a0243dffe63fd540d70
Uwem Uke <slyart147@gmail.com>
2019-11-26 15:52:48 +0100

    feed

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,26 +1,53 @@
 const { Pool } = require('pg');
 
-//const pool = new Pool();
+const pool = new Pool();
 
-const pool = new Pool({
+/*const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
-});
+});*/
 
-const text = 'SELECT imageid as Id, createdon, title, imageurl as url_or_article, userid as authorId FROM images UNION ALL SELECT articleid, createdon, title, article, userid FROM articles ORDER BY createdon DESC LIMIT 20';
+//const text = 'SELECT imageid as Id, createdon, title, imageurl as url_or_article, userid as authorId FROM images UNION ALL SELECT articleid, createdon, title, article, userid FROM articles ORDER BY createdon DESC LIMIT 20';
+
+const union = `WITH A as (SELECT i.imageid as itemid, i.createdon, i.title, i.imageurl as item, i.userid, u.firstname as author 
+FROM images i JOIN users u ON i.userid = u.userid UNION ALL SELECT a.articleid, a.createdon, a.title, a.article, a.userid, u.firstname 
+FROM articles a JOIN users u ON a.userid = u.userid ORDER BY createdon DESC) 
+SELECT row_number() over (order by (select NULL)) as id, itemid, createdon, title, item, userid, author 
+INTO feed FROM A`;
 
 exports.getFeed = (request, response) => {
-  pool.query(text, (error, res) => {
+  console.log('response', response.body)
+  /*pool.query('DROP TABLE IF EXISTS feed', (error, res) => {
     if (error) {
       // throw error
       response.status(400).json({
         status: 'error',
         error: error.stack,
       });
-    }
-    response.status(200).json({
-      status: 'success',
-      data: res.rows,
-    });
-  });
+    }*/
+    pool.query(union, (erro, resp) => {
+      if (erro) {
+        // throw error
+        return response.status(400).json({
+          status: 'error',
+          error: erro.stack,
+        });
+      }
+      pool.query('SELECT * FROM feed', (error3, result) => {
+        if (erro) {
+          // throw error
+          return response.status(400).json({
+            status: 'error',
+            error: error3.stack,
+          });
+        }
+        console.log('result', result.rows)
+        return response.status(200).json({
+          status: 'success',
+          data: result.rows,
+        });
+      })
+    })
+    
+  //});
 };
--- routes/feed.js
+++ routes/feed.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../middleware/auth');
+//const auth = require('../middleware/auth');
 
 const feedCtrl = require('../controllers/feed');
 
 
-router.get('/feed', auth, feedCtrl.getFeed);
+router.get('/feed', /*auth,*/ feedCtrl.getFeed);
 
 module.exports = router;
