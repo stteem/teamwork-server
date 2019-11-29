f7d233f52a155721baa3893018051c59eeedeb99
Uwem Uke <slyart147@gmail.com>
2019-11-29 04:21:05 +0100

    config

--- controllers/user.js
+++ controllers/user.js
@@ -3,12 +3,12 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 
-const pool = new Pool();
+//const pool = new Pool();
 
-/*const pool = new Pool({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
-});*/
+});
 
 
 exports.getUsers = (request, response, next) => {
