796ea754caa5b15b025a9aa060f13b01740079e9
Uwem Uke <slyart147@gmail.com>
2019-12-04 17:36:06 +0100

    .env

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
-//const pool = new Pool();
+const pool = new Pool();
 
-const pool = new Pool({
+/*const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
-});
+});*/
 
 //const text = 'SELECT imageid as Id, createdon, title, imageurl as url_or_article, userid as authorId FROM images UNION ALL SELECT articleid, createdon, title, article, userid FROM articles ORDER BY createdon DESC LIMIT 20';
 
