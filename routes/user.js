cdce32e078a4138106efc0d219d6755c944fe172
Uwem Uke <slyart147@gmail.com>
2020-02-03 05:28:33 +0100

    users

--- routes/user.js
+++ routes/user.js
@@ -7,6 +7,7 @@ const auth = require('../middleware/auth');
 
 const userCtrl = require('../controllers/user');
 
+router.get('/users', userCtrl.getUsers);
 router.post('/auth/create-user', auth, userCtrl.createUser);
 router.post('/auth/signin', userCtrl.login);
 
